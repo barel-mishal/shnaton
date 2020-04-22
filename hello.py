82b8716d7de02415dab995551a6d6eaf20508094
Your Name <you@example.com>
2020-04-22 20:15:30 +0300

    chenge

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import io
+
 
 link = requests.get("https://shnaton.huji.ac.il/index.php?peula=Simple&maslul=0&shana=0&year=2020&course=71012")
-print(link)
-print(link.text)
+with io.open("out1.html", "w", encoding="utf-8") as f:
+    f.write(link.text)
+f.close()
