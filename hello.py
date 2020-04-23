919e9ac665244ca982201ecc16bfc0cb88b3743f
Your Name <you@example.com>
2020-04-23 09:16:09 -0700

    Deletes commeted out code

--- hello.py
+++ hello.py
@@ -1,14 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-# print(BeautifulSoup)
-
-
 # Hi Barel, whats up?
 link = requests.get("https://shnaton.huji.ac.il/index.php?peula=Simple&maslul=0&shana=0&year=2020&course=71012")
 
 soup = BeautifulSoup(link.text, features="html.parser")
 print(soup.find(id="mainmenu"))
-
-# print(link)
-# print(link.text)
