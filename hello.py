a09714fce57b76e62aacd255b429f7f312e6f8f7
Your Name <you@example.com>
2020-04-25 00:14:42 +0300

    make a list and loop over it to get the date

--- hello.py
+++ hello.py
@@ -1,16 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import io
+import re
 
 
-# Hi Barel, whats up?
 link = requests.get("https://shnaton.huji.ac.il/index.php?peula=CourseD&course=71012&detail=examDates&year=2020&line=&faculty=8&maslul=0")
 text = link.text
 
 soup = BeautifulSoup(text, features="html.parser")
 # print(soup.find(id="mainmenu"))
-print(soup.select("td.courseTab_content"))
+table = soup.select("td.courseTab_content")
+list_datatable = []
+for i in table:
+	list_datatable = i.select("td.courseTab_td")
+for i in list_datatable[0::6]:
+	print(i) # to try regular exprtion to catch the date
 
+
+	
 with io.open("out1.html", "w", encoding="utf-8") as f:
     f.write(text.replace("windows-1255", "utf-8"))
 f.close()
+
+
